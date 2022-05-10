1a48a0f8f25012bf4f1b3b371263b8269b2f124f
Lawrence LCI <lcheunglci@gmail.com>
2022-05-09 22:18:13 -0700

    Add project files.

---
+++ KaijuApp.Domain/Ability.cs
@@ -0,0 +1,12 @@
+﻿namespace KaijuApp.Domain
+{
+    public class Ability
+    {
+        public int Id { get; set; }
+        public string Detail { get; set; }
+        public Kaiju Kaiju { get; set; }
+        public int KaijuId { get; set; }
+
+
+    }
+}
---
+++ KaijuApp.Domain/Kaiju.cs
@@ -0,0 +1,10 @@
+﻿namespace KaijuApp.Domain
+{
+    public class Kaiju
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Ability> Effects { get; set; } = new List<Ability>();
+    }
+}
