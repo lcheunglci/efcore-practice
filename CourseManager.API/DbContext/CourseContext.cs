40f0826f77b3193fd920fc6f46721da53bf6c266
Lawrence LCI <lcheunglci@gmail.com>
2022-08-28 23:13:41 -0700

    Add CourseManager API for adding unit tests

---
+++ CourseManager.API/DbContext/CourseContext.cs
@@ -0,0 +1,17 @@
+﻿using CourseManager.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseManager.API.DbContexts
+{
+    public class CourseContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public CourseContext()
+        { }
+    }
+}
---
+++ CourseManager.API/Program.cs
@@ -0,0 +1,47 @@
+using CourseManager.API.DbContexts;
+using CourseManager.API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<AuthorRepository>();
+
+var app = builder.Build();
+
+// migrate the database.  Best practice = in Main, using service scope
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        CourseContext? context = scope.ServiceProvider.GetService<CourseContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
